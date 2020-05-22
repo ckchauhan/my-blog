37a87479a543ee82a5c268459f964b23f8fc04a0
Chandrakant <chandrakant.rots@gmail.com>
2020-05-22 10:56:26 -0500

    First commit for my blog

---
+++ src/build/precache-manifest.3deda00fb99ebdc4f321b848728100dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c8b562a1b1186d48b33e4ca1e1d77c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5093aeca38586b632853",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "3bc968ee9f5afd94e983",
+    "url": "/static/js/2.9533a439.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9533a439.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5093aeca38586b632853",
+    "url": "/static/js/main.98916057.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
